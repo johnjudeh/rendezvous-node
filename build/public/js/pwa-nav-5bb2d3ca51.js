f5f5ca0750adb7dd5053b774150717c715e35453
John Judeh <johnjudeh@hotmail.com>
2018-03-20 23:48:16 +0000

    Added event listener only to a tags with data-href attr

--- build/public/js/pwa-nav-5bb2d3ca51.js
+++ build/public/js/pwa-nav-5bb2d3ca51.js
@@ -1,11 +1,13 @@
-const anchorTags = document.querySelectorAll('a');
+'use strict';
+
+var anchorTags = document.querySelectorAll('a');
 
 // Adds event to keep web app in full-screen mode
-anchorTags.forEach((anchor) => {
+anchorTags.forEach(function (anchor) {
   // Ensures that buttons without links do not get this event
   if (anchor.dataset.href) {
-    anchor.addEventListener('click', () => {
+    anchor.addEventListener('click', function () {
       location.href = anchor.dataset.href;
-    })
+    });
   }
-})
+});
