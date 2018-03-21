2cc34a582f7ca28dc72142d7852e01980bb6c0fa
John Judeh <johnjudeh@hotmail.com>
2018-03-21 23:05:10 +0000

    Removed comment that seems to be causing bug on ios from pwa-nav.js

--- build/public/js/pwa-nav-5ba8b79b18.js
+++ build/public/js/pwa-nav-5ba8b79b18.js
@@ -4,7 +4,6 @@ var anchorTags = document.querySelectorAll('a');
 
 // Adds event to keep web app in full-screen mode
 anchorTags.forEach(function (anchor) {
-  // Ensures that buttons without links do not get this event
   if (anchor.dataset.href) {
     anchor.addEventListener('click', function () {
       location.href = anchor.dataset.href;
--- public/js/pwa-nav.js
+++ public/js/pwa-nav.js
@@ -2,10 +2,9 @@ const anchorTags = document.querySelectorAll('a');
 
 // Adds event to keep web app in full-screen mode
 anchorTags.forEach((anchor) => {
-  // Ensures that buttons without links do not get this event
   if (anchor.dataset.href) {
     anchor.addEventListener('click', () => {
       location.href = anchor.dataset.href;
-    })
+    });
   }
-})
+});
